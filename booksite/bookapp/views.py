f02e407c230a3cfa7c3eb54b1bc0d31f25a437d2
Ashok-reddy340 <ashoklakki94.com>
2021-06-14 20:14:19 +0530

    create function for adding new book by user

--- booksite/bookapp/views.py
+++ booksite/bookapp/views.py
@@ -15,4 +15,13 @@ def detail(request,book_id):
     book = Book.objects.get(id=book_id)
     return render(request,'bookapp/detail.html',{'book':book})
 def add_book(request):
+    if request.method=="POST":
+        name = request.POST.get('name',)
+        author = request.POST.get('author',)
+        price = request.POST.get('price',)
+        book_image = request.FILES['book_image']
+
+        book = Book(name=name,author=author,price=price,book_image=book_image)
+        book.save()
+
     return render(request,'bookapp/add_book.html')
